3534a1a2af6df50d62051aa2cb8e0425840c1fff
Antoine Delcourte <adelcourte.pro@gmail.com>
2023-05-26 14:24:44 +0200

    Cleaning files from ionic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,10 @@
-'use client'
-
-import { setupIonicReact } from '@ionic/react'
-
-setupIonicReact()
-
-import '@ionic/react/css/core.css'
 import '@/styles/app.scss'
 
-// export const metadata = {
-//     title: 'Larry',
-//     description:
-//         'Your personal web keeper to save, organize and share your links.',
-// }
+export const metadata = {
+    title: 'Larry',
+    description:
+        'Your personal web keeper to save, organize and share your links.',
+}
 
 export default function RootLayout({
     children,
