0dedb960c9d08210d9a07a1df91d49e6d6c92d17
Antoine Delcourte <adelcourte.pro@gmail.com>
2023-05-27 15:33:20 +0200

    Force to login if not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+
+import supabase from '@/lib/supabase/client'
 
 export default function HHome() {
     const router = useRouter()
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+    const getProfile = async () => {
+        const {
+            data: { user },
+        } = await supabase.auth.getUser()
+
+        if (user) {
+            setIsLoggedIn(true)
+        } else {
+            setIsLoggedIn(false)
+        }
+    }
 
     useEffect(() => {
-        router.push('/tabs/pages')
+        getProfile()
+
+        if (isLoggedIn) {
+            router.push('/tabs/pages')
+        } else {
+            router.push('/login')
+        }
     }, [])
 
     return null
